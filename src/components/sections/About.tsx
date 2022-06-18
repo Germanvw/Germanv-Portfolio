26847cd5051a7ba2273789c3ca8a284bed39ea0c
Germanvw <germanvazquezw99@gmail.com>
2022-06-18 04:08:33 -0300

    About Me Section v1
    About Me Section v1

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -6,14 +6,18 @@ import {
   List,
   ListIcon,
   ListItem,
+  Spacer,
   Stack,
   Text,
 } from '@chakra-ui/react';
 
+import { AiFillCaretRight } from 'react-icons/ai';
+import { skills } from '../../data/skills';
+
 export const About = () => {
   return (
     <Stack width='100%'>
-      <Stack direction='row' alignItems='center'>
+      <Stack direction='row' alignItems='center' mb={10}>
         <Heading minW={200} as='kbd'>
           About Me
         </Heading>
@@ -21,53 +25,54 @@ export const About = () => {
       </Stack>
       <Stack direction={{ base: 'column', lg: 'row' }} alignItems='center'>
         <Stack direction='column'>
-          <Text fontSize='1.15rem' fontWeight='400' lineHeight='200%'>
-            Hi! My name is German and im a 23yo FullStack Developer who is
-            passionate about learning the latest technologies and pushing my
-            skills every day.
-          </Text>
-          <Text fontSize='1.15rem' fontWeight='400' lineHeight='200%'>
-            My first contact with programming was a subject called Introduction
-            to Programming that i took while going the university to become a
-            Civil Engineer.
-          </Text>
-          <Text fontSize='1.15rem' fontWeight='400' lineHeight='200%'>
-            Since then i become in love with coding, changed careers and
-            graduated from the UTN as a Technician in Programming/Computer
-            Systems in Dec 2021. At the beggining of that year i also began a
-            self-learning journey to become a fullstack developer while
-            finishing the university. Since a few months ago im working as a
-            React Developer at Cedeira Software Factory.
-          </Text>
-          <Text
-            fontSize='1.15rem'
-            fontWeight='400'
-            lineHeight='200%'
-            color='primary'
-          >
-            Few technologies that i use in my daily life are:
-          </Text>
+          <Box>
+            <Text fontSize='1.1rem' fontWeight='400' lineHeight='200%'>
+              Hi! My name is German and im a 23yo FullStack Developer who is
+              passionate about learning the latest technologies and pushing my
+              skills every day.
+            </Text>
+            <Text fontSize='1.1rem' fontWeight='400' lineHeight='200%'>
+              My first contact with programming was a subject called
+              Introduction to Programming that i took while going the university
+              to become a Civil Engineer in 2019.
+            </Text>
+            <Text fontSize='1.1rem' fontWeight='400' lineHeight='200%'>
+              Since then i become in love with coding, changed careers and
+              graduated from the UTN as a Technician in Programming/Computer
+              Systems in Dec 2021. At the beggining of that year i also began a
+              self-learning journey to become a fullstack developer while
+              finishing the university.
+            </Text>
+            <Text fontSize='1.1rem' fontWeight='400' lineHeight='200%'>
+              Right now im working as a React Developer at Cedeira Software
+              Factory.
+            </Text>
+          </Box>
+          <Box py={6}>
+            <Text fontSize='1.15rem' fontWeight='400' lineHeight='200%'>
+              Few technologies that i use in my daily life are:
+            </Text>
+          </Box>
+          <Box>
+            <Stack direction='row' justifyContent='between'>
+              {skills?.map((col, i) => (
+                <>
+                  <Stack key={i}>
+                    <List spacing={3}>
+                      {col?.map((skill) => (
+                        <ListItem key={skill}>
+                          <ListIcon as={AiFillCaretRight} color='primary' />
+                          {skill}
+                        </ListItem>
+                      ))}
+                    </List>
+                  </Stack>
+                  {i === 0 && <Spacer></Spacer>}
+                </>
+              ))}
+            </Stack>
+          </Box>
         </Stack>
-        <List spacing={3}>
-          {/* <ListItem>
-            <ListIcon as={MdCheckCircle} color='green.500' />
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit
-          </ListItem>
-          <ListItem>
-            <ListIcon as={MdCheckCircle} color='green.500' />
-            Assumenda, quia temporibus eveniet a libero incidunt suscipit
-          </ListItem>
-          <ListItem>
-            <ListIcon as={MdCheckCircle} color='green.500' />
-            Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
-          </ListItem>
-          {/* You can also use custom icons from react-icons */}
-          <ListItem>
-            <ListIcon as={MdSettings} color='green.500' />
-            Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
-          </ListItem> */}
-        </List>
-
         <Img
           src='/images/portrait.jpg'
           borderRadius='full'
---
+++ src/data/skills.ts
@@ -0,0 +1,4 @@
+export const skills = [
+  ['JavaScript (ES6+)', 'TypeScript', 'React', 'NodeJs'],
+  ['SqlServer & MongoDB', '.Net Core 6', 'Bootstrap, ChakraUi & Scss'],
+];
