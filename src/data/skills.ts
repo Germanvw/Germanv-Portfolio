785d51042db0bb15e51392443de22cc47099b891
Germanvw <germanvazquezw99@gmail.com>
2022-07-15 10:58:35 -0300

    React Native Skill Added
    React Native Skill Added

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,4 +1,4 @@
 export const skills = [
-  ['JavaScript (ES6+)', 'TypeScript', 'React', 'NodeJs'],
+  ['JavaScript (ES6+)', 'TypeScript', 'React', 'React Native', 'NodeJs'],
   ['SqlServer & MongoDB', '.Net Core 6', 'Bootstrap, ChakraUi & Scss'],
 ];
