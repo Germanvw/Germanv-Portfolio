ff367bc4c755375c66a1933488641c0af3e94f64
Germanvw <germanvazquezw99@gmail.com>
2023-07-30 14:50:41 -0300

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,17 @@
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
-import { Template } from '../components/Template';
-import { customTheme } from '../styles/theme';
-import '../styles/global.css';
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import { Template } from "../components/Template";
+import { customTheme } from "../styles/theme";
+import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <title>German Vazquez</title>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700&family=Roboto+Slab&display=swap'
-          rel='stylesheet'
-        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
       </Head>
       <ChakraProvider resetCSS theme={customTheme}>
         <ColorModeScript
