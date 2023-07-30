ca367db3fb963c6a08bd51316168ba55c1aab04a
Germanvw <germanvazquezw99@gmail.com>
2023-07-30 14:38:50 -0300

    fix dependency issue

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,17 +1,16 @@
-import { extendTheme } from '@chakra-ui/react';
-import { createBreakpoints } from '@chakra-ui/theme-tools';
+import { extendTheme } from "@chakra-ui/react";
 
-const breakpoints = createBreakpoints({
-  xs: '0px',
-  sm: '576px',
-  md: '768px',
-  lg: '992px',
-  xl: '1200px',
-  xxl: '1600px',
-});
+const breakpoints = {
+  xs: "0px",
+  sm: "576px",
+  md: "768px",
+  lg: "992px",
+  xl: "1200px",
+  xxl: "1600px",
+};
 
 const config = {
-  initialColorMode: 'dark',
+  initialColorMode: "dark",
   useSystemColorMode: false,
   breakpoints,
 };
@@ -19,11 +18,11 @@ const config = {
 export const customTheme = extendTheme({
   config,
   colors: {
-    primary: '#ED3C58',
-    secondary: '#C62E46',
-    grey: '#F1F5F7',
-    svgLight: '#F1F5F7',
-    svgDark: '#1c1c1c',
+    primary: "#ED3C58",
+    secondary: "#C62E46",
+    grey: "#F1F5F7",
+    svgLight: "#F1F5F7",
+    svgDark: "#1c1c1c",
   },
   fonts: {
     heading: '"Roboto", sans-serif',
